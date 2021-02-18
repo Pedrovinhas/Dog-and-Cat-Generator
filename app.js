8e08661d649b53b016184b182175561ae2aa8eaa
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-17 22:17:08 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const catButton = document.getElementById('dog') 
+const dogButton = document.getElementById('cat')
+const imgDog = document.getElementById('cachorro')
+const imgCat = document.getElementById('gatinho')
+
+catButton.addEventListener('click', getRandomCat)
+dogButton.addEventListener('click', getRandomDog)
+
+
+function getRandomCat () {
+
+    
+}
