2011167415f56cb43840aa5d7d76dec4f9cd8486
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-02-17 23:52:16 -0300

    added fetch api files

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const catButton = document.getElementById('dog') 
-const dogButton = document.getElementById('cat')
-const imgDog = document.getElementById('cachorro')
-const imgCat = document.getElementById('gatinho')
+const catButton = document.getElementById('cat');
+const dogButton = document.getElementById('dog');
+const imgDog = document.getElementById('cachorro');
+const imgCat = document.getElementById('gatinho');
 
-catButton.addEventListener('click', getRandomCat)
-dogButton.addEventListener('click', getRandomDog)
+catButton.addEventListener('click', getRandomCat);
+dogButton.addEventListener('click', getRandomDog);
 
 
 function getRandomCat () {
+    fetch('https://aws.random.cat/meow')
+    .then (res => res.json())
+    .then ( data => {
+        imgCat.innerHTML = `<img src="${data.file}"/>`
+    })
+} 
 
-    
-}
+function getRandomDog () {
+    fetch('https://random.dog/woof.json')
+    .then (res => res.json())
+    .then ( data => {
+        imgDog.innerHTML = `<img src="${data.url}"/>`
+    })
+}    
